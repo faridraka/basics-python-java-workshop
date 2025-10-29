f1327d0a00eba38c2debfd051947ca3fb46caba4
faridraka <faridrakapratama@gmail.com>
2025-10-29 10:58:59 +0700

    Solve challenges 2 py

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,14 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
-
+age = int(input("Masukkan umur kamu: "))
 # 2. TODO Tentukan kategori berdasarkan usia
+
+if age >= 60:
+  print("Lansia")
+elif age >= 18:
+  print("Dewasa")
+elif age >= 12:
+  print("Remaja")
+else:
+  print("Anak-anak")
