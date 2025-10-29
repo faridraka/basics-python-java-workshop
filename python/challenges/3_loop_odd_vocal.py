803830ff7a8b2f54df8a00bd4394ab9b0b7ec436
faridraka <faridrakapratama@gmail.com>
2025-10-29 11:17:53 +0700

    Solve challenges 3 py

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,5 +1,10 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
 odd_numbers = []
+
+for i in range(16):
+  if i % 2 == 1:
+    odd_numbers.append(i)
+
 print(odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
@@ -7,5 +12,9 @@ word = input("Masukkan kata: ").lower()
 
 vowels = "aiueo"
 count = 0
+
+for char in word:
+  if char in vowels:
+    count += 1  
 
 print("Jumlah huruf vokal:", count)
