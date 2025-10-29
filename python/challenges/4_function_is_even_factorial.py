3953813cc9fc50469b57f0443599c79223239f15
faridraka <faridrakapratama@gmail.com>
2025-10-29 11:29:10 +0700

    Solve challenges 4 py

--- python/challenges/4_function_is_even_factorial.py
+++ python/challenges/4_function_is_even_factorial.py
@@ -1,6 +1,6 @@
 # TODO: Fungsi untuk memeriksa apakah sebuah bilangan genap
 def is_even(num):
-    pass
+    return True if num % 2 == 0 else False
 
 # Tes fungsi
 print(is_even(4))   # True
@@ -8,7 +8,10 @@ print(is_even(7))   # False
 
 # TODO: Fungsi untuk menghitung faktorial
 def factorial(n):
-    pass
+    if n == 0 or n == 1:
+        return 1
+    
+    return n * factorial(n - 1)
 
 # Tes fungsi
 print(factorial(5))  # 120
